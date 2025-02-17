97ac938c69f476d930e127de19d221144c8fc656
ekalavyaaditya <ekalavyaaditya@gmail.com>
2025-02-17 23:00:00 +0530

    updated the backend end point

--- src/utill/index.js
+++ src/utill/index.js
@@ -4,7 +4,7 @@ import { jwtDecode } from "jwt-decode";
 const isDevelopment = window.location.hostname.includes("localhost");
 
 const getServer = () => {
-  return isDevelopment ? "http://localhost:5000" : "http://130.211.208.130:5000 ";
+  return isDevelopment ? "http://localhost:5000" : "http://34.41.56.207:5000";
 };
 
 const decodeUser = () => {
