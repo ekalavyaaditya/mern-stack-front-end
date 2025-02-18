f97fc610ffe2418c021260fb49aea0c5ff515cb8
aditya <129822187+ekalavyaaditya@users.noreply.github.com>
2025-02-18 11:24:22 +0530

    Update index.js do git pull than start the request
    the backend end point was not static it was changes ever time i stop and start the instance it was changeing the ip address 
    note:-i hope this works

--- src/utill/index.js
+++ src/utill/index.js
@@ -2,9 +2,9 @@
 import { jwtDecode } from "jwt-decode";
 
 const isDevelopment = window.location.hostname.includes("localhost");
-
+//  this is the static ip address for the back-end http://34.55.53.146:5000
 const getServer = () => {
-  return isDevelopment ? "http://localhost:5000" : "http://34.41.56.207:5000";
+  return isDevelopment ? "http://localhost:5000" : "http://34.55.53.146:5000";
 };
 
 const decodeUser = () => {
